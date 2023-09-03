cf6198e80531d15ba364254ee296fd906d6b0ed6
Наталья Наталья <nlkim.hse@gmail.com>
2023-09-03 11:06:46 +0900

    Updated removeProduct1 method to interact correctly with the element

--- src/main/java/Pages/CompareListPage.java
+++ src/main/java/Pages/CompareListPage.java
@@ -2,14 +2,15 @@ package Pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 
-    public class CompareListPage {
+public class CompareListPage {
         private WebDriver driver;
         private By selectedProduct2 = By.xpath("//div[@class='zvRJM']");
         private By selectedProduct2Price = By.cssSelector("[data-auto='mainPrice']");
         private By selectedProduct1 = By.xpath("(//div[@class='zvRJM'])[2]");
         private By selectedProduct1Price = By.xpath("(//span[@data-auto='mainPrice'])[2]");
-        private By product1DeleteButton = By.xpath("(//div[@aria-label='Удалить'])[2]");
+        private By product1DeleteButton = By.xpath("(//div[@role='button'])[6]");
         private By removeListButton = By.cssSelector("button._1KpjX._1KU3s");
 
         public CompareListPage(WebDriver driver){
@@ -30,22 +31,21 @@ import org.openqa.selenium.WebDriver;
             return Integer.valueOf(cleanPriceString);
         }
 
-        // 1. Удалить товар производителя «Whiskas» из сравнения
         public void removeProduct1() {
-                driver.findElement(product1DeleteButton).click();
+            Actions actions = new Actions(driver);
+            actions.moveToElement(driver.findElement(selectedProduct1), 10, 10).click(driver.findElement(product1DeleteButton)).build().perform();
+
+            driver.findElement(product1DeleteButton).click();
         }
 
-        // 2. Нажать на «Удалить список» (значок мусорного бака)
         public void clickRemoveList() {
             driver.findElement(removeListButton).click();
         }
 
-        // Проверить, что товар производителя «Whiskas» не отображается
         public boolean isProduct1Displayed() {
             return !driver.findElement(selectedProduct1).isDisplayed();
         }
 
-        // Проверить, что товары не отображаются
         public boolean areProductsDisplayed() {
             return !driver.findElement(selectedProduct1).isDisplayed() && !driver.findElement(selectedProduct2).isDisplayed();
         }
